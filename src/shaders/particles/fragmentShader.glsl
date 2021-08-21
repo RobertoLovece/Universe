be6f3ce62a58758dd95b8c0c7141544e0ac5377b
rlovece <rlovece@hotmail.co.uk>
2021-08-22 00:50:48 +0100

    8th Attempt to fix IOS fragment shader issues - Reremoved palette added just vRand

--- src/shaders/particles/fragmentShader.glsl
+++ src/shaders/particles/fragmentShader.glsl
@@ -4,7 +4,7 @@
 // varying vec3 vPosition;
 // float PI = 3.141592653589793238;
 varying float vRand;
-uniform vec3 palette[5];
+// uniform vec3 palette[5];
 
 void main()	{
 	//--- vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
@@ -12,9 +12,9 @@ void main()	{
 
 	// float disc = smoothstep(0.4,0.45,dist);
 	// if(disc>0.01) discard;
-	vec3 color = palette[int(vRand)];
+	// vec3 color = palette[int(vRand)];
 	//--- vec3 color = vec3(1.);
-	// gl_FragColor = vec4(1.0,1.0,1.0,vRand*0.1);
-	gl_FragColor = vec4(1.0,1.0,1.0,0.1);
-	gl_FragColor = vec4(color,0.1);
+	gl_FragColor = vec4(1.0,1.0,1.0,vRand*0.1);
+	// gl_FragColor = vec4(1.0,1.0,1.0,0.1);
+	// gl_FragColor = vec4(color,0.1);
 }
