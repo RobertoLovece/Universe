092c27bffd337d41d85963852371d7f6afe339e0
rlovece <rlovece@hotmail.co.uk>
2021-08-21 22:50:36 +0100

    IOS fragment shader fix attempt #1

--- src/shaders/particles/vertexShader.glsl
+++ src/shaders/particles/vertexShader.glsl
@@ -2,17 +2,15 @@ uniform float time;
 uniform float size;
 varying vec2 vUv;
 varying vec3 vPosition;
-
-uniform sampler2D texture1;
 attribute float rands;
 varying float vRand;
 float PI = 3.141592653589793238;
 
 void main() {
   vUv = uv;
-  vRand = floor(rands*5.);
-  vec4 mvPosition = modelViewMatrix * vec4( position, 1. );
-  gl_PointSize = 1000. * ( 1. / - mvPosition.z );
+  vRand = floor(rands*5.0);
+  vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+  gl_PointSize = 1000. * ( 1.0 / - mvPosition.z );
   gl_PointSize = vRand*3.0*size;
   gl_Position = projectionMatrix * mvPosition;
 }
