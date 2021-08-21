b5934e63f047c449f32b20d20f54762612219181
rlovece <rlovece@hotmail.co.uk>
2021-08-21 02:58:55 +0100

    Added responsive github button link

--- src/index_css.js
+++ src/index_css.js
@@ -8,9 +8,11 @@ export function cssInit() {
     const border = CSSRulePlugin.getRule('.content:before');
     const h1 = document.querySelector('h1');
     const p = document.querySelector('p');
+    const u = document.querySelector('u')
     const tl = gsap.timeline();
 
-    tl.from(border, { dealy: 1, duration: 4, cssRule: { scaleX: 0 } })
+    tl.from(border, { delay: 1, duration: 4, cssRule: { scaleX: 0 } })
     tl.to(h1, { duration: 2, clipPath: 'polygon( 0 0, 100% 0, 100% 100%, 0% 100% )', y: '30px' }, "-=3")
     tl.to(p, { duration: 4, clipPath: 'polygon( 0 0, 100% 0, 100% 100%, 0% 100% )', y: '30px' }, "-=2")
+    tl.to(u, { duration: 4, clipPath: 'polygon( 0 0, 100% 0, 100% 100%, 0% 100% )', y: '30px' }, "-=2")
 }
