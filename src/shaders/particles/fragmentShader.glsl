aa9f301dcd5e7a6d2354dbf13363a4212c3b3061
rlovece <rlovece@hotmail.co.uk>
2021-08-22 01:02:06 +0100

    11th Attempt to fix IOS fragment shader issues - Found issue, redeploying without line

--- src/shaders/particles/fragmentShader.glsl
+++ src/shaders/particles/fragmentShader.glsl
@@ -1,20 +1,22 @@
-// uniform float time;
-// uniform vec4 resolution;
-// varying vec2 vUv;
-// varying vec3 vPosition;
-// float PI = 3.141592653589793238;
+uniform float time;
+uniform vec4 resolution;
+varying vec2 vUv;
+varying vec3 vPosition;
+float PI = 3.141592653589793238;
 varying float vRand;
 uniform vec3 palette[5];
 
 void main()	{
 	//--- vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
-	// float dist = length(gl_PointCoord.xy - vec2(0.5));
+	float dist = length(gl_PointCoord.xy - vec2(0.5));
 
-	// float disc = smoothstep(0.4,0.45,dist);
-	// if(disc>0.01) discard;
-	vec3 color = palette[int(vRand)];
+	float disc = smoothstep(0.4,0.45,dist);
+	if(disc>0.01) discard;
+
+	// this line is causing issues
+	// vec3 color = palette[int(vRand)];
 	//--- vec3 color = vec3(1.);
-	gl_FragColor = vec4(0.5,1.0,1.0,vRand*0.1);
+	gl_FragColor = vec4(1,1.0,1.0,vRand*0.1);
 	// gl_FragColor = vec4(1.0,1.0,1.0,0.1);
 	// gl_FragColor = vec4(color,0.1);
 }
